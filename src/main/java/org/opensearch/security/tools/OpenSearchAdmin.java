eefb7d9ad52030dd635f60aa4571267311926c32
Himanshu Setia <setiah@amazon.com>
2022-05-13 17:57:34 -0700

    Making OpenSearchAdmin non-singleton and fixing related UTs
    Signed-off-by: Himanshu Setia <setiah@amazon.com>

--- src/main/java/org/opensearch/security/tools/OpenSearchAdmin.java
+++ src/main/java/org/opensearch/security/tools/OpenSearchAdmin.java
@@ -41,17 +41,10 @@ public class OpenSearchAdmin {
 
     private static OpenSearchAdmin opensearchAdminInstance = null;
 
-    private OpenSearchAdmin() {
+    public OpenSearchAdmin() {
         System.out.println("A tool for installing and configuring OpenSearch security");
         initialize();
     }
-
-//    public static OpenSearchAdmin getInstance() {
-//        if(null == opensearchAdminInstance)
-//            opensearchAdminInstance = new OpenSearchAdmin();
-//
-//        return opensearchAdminInstance;
-//    }
 
     protected void logInvalidUsage() {
         System.out.println("ERR: Invalid Usage");
@@ -63,6 +56,7 @@ public class OpenSearchAdmin {
         System.out.println("Check usage with:\nopensearch-admin [action] --help");
         System.out.println("If you face any issues, please report at https://github.com/opensearch-project/security/issues/new/choose");
     }
+
 
     public void execute(String[] args) {
         if(args.length == 0 || (args.length == 1 && helpOptions.contains(args[0]))) {
@@ -87,24 +81,5 @@ public class OpenSearchAdmin {
 
     public static void main(String[] args) throws Exception {
         (new OpenSearchAdmin()).execute(args);
-//        OpenSearchAdmin openSearchAdmin = getInstance();
-//        if(args.length == 0 || (args.length == 1 && openSearchAdmin.helpOptions.contains(args[0]))) {
-//            openSearchAdmin.printHelp();
-//            return;
-//        }
-//        else if(!openSearchAdmin.commands.containsKey(args[0])) {
-//            openSearchAdmin.logInvalidUsage();
-//            return;
-//        }
-//        else if(args.length > 1 && openSearchAdmin.helpOptions.contains(args[1])) {
-//            openSearchAdmin.commands.get(args[0]).usage();
-//            return;
-//        }
-//
-//        try {
-//            openSearchAdmin.commands.get(args[0]).main(Arrays.copyOfRange(args, 0, args.length));
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
     }
 }
